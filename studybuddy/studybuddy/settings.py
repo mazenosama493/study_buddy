3bf4638fd52e2fcaf77c7b8afc1bbfe612c46c66
unknown <mazenosama987654321@gmail.com>
2025-04-05 04:34:34 +0200

    hi

--- studybuddy/studybuddy/settings.py
+++ studybuddy/studybuddy/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'studybuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'study_buddy_dbs',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
